39485e04ee1fc2d66a90ade713d9ed38508d0f7c
toda-axia <hiroyuki.toda@axia-works.com>
2020-04-18 13:33:26 +0900

    firebaseでアプリ起動ログ出力

--- app/src/main/java/com/wumpuss/qiitaclient/ui/MainActivity.kt
+++ app/src/main/java/com/wumpuss/qiitaclient/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.wumpuss.qiitaclient.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wumpuss.qiitaclient.R
+import com.wumpuss.qiitaclient.utils.AnalyticsUtils
 import com.wumpuss.qiitaclient.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity(), ConfirmDeleteListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AnalyticsUtils.sendGameStartLog(baseContext)
 
         fab.setOnClickListener {
             InputSearchTagDialog().show(supportFragmentManager, InputSearchTagDialog.TAG)
---
+++ app/src/main/java/com/wumpuss/qiitaclient/utils/AnalyticsUtils.kt
@@ -0,0 +1,31 @@
+package com.wumpuss.qiitaclient.utils
+
+import android.content.Context
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+object AnalyticsUtils {
+    private var firebaseAnalytics: FirebaseAnalytics? = null
+
+    private fun getAnalytics(context: Context): FirebaseAnalytics {
+        firebaseAnalytics?.let {
+            return it
+        } ?: run {
+            return FirebaseAnalytics.getInstance(context).apply {
+                firebaseAnalytics = this
+            }
+        }
+    }
+
+    private fun sendLog(context: Context, eventName: String, bundle: Bundle) {
+        getAnalytics(context).logEvent(eventName, bundle)
+    }
+
+    fun sendGameStartLog(context: Context) {
+        val bundle = Bundle().apply {
+            putString("screen", "home")
+            putString("item_id", "app_launch")
+        }
+        sendLog(context, "game_select", bundle)
+    }
+}
