baf17486417c59467372afa343763e0c3b1e8b51
toda-axia <hiroyuki.toda@axia-works.com>
2020-04-26 18:14:19 +0900

    検索結果画面に戻るボタン実装

--- app/src/main/java/com/wumpuss/qiitaclient/ui/SearchResultActivity.kt
+++ app/src/main/java/com/wumpuss/qiitaclient/ui/SearchResultActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
 import com.wumpuss.qiitaclient.R
 import com.wumpuss.qiitaclient.viewmodel.MainViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -28,5 +28,17 @@ class SearchResultActivity : AppCompatActivity() {
         viewModel.tag = tag
 
         setContentView(R.layout.activity_search_result)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_back, menu)
+
+        val backMenuItem = menu.findItem(R.id.back_icon)
+        backMenuItem.setOnMenuItemClickListener {
+            this.finish()
+            true
+        }
+        return true
     }
 }
