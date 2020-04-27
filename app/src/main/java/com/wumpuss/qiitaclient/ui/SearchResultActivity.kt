cc2aa228ca21e5c999223358315b883936205a82
toda-axia <hiroyuki.toda@axia-works.com>
2020-04-27 20:01:37 +0900

    戻るボタンの実装と修正

--- app/src/main/java/com/wumpuss/qiitaclient/ui/SearchResultActivity.kt
+++ app/src/main/java/com/wumpuss/qiitaclient/ui/SearchResultActivity.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.view.MenuItem
 import com.wumpuss.qiitaclient.R
 import com.wumpuss.qiitaclient.viewmodel.MainViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -25,20 +25,16 @@ class SearchResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val tag = intent.getStringExtra(INTENT_EXTRA_TAG)!!
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         viewModel.tag = tag
 
         setContentView(R.layout.activity_search_result)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_back, menu)
-
-        val backMenuItem = menu.findItem(R.id.back_icon)
-        backMenuItem.setOnMenuItemClickListener {
-            this.finish()
-            true
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == android.R.id.home) {
+            finish()
         }
-        return true
+        return super.onOptionsItemSelected(item)
     }
 }
